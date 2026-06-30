41e4d79c2224d5adbf24f70ff78570bc983d5307
Autobet-ui <sagaronlyfreefire1@gmail.com>
2026-06-30 12:44:26 +0530

    Fix: missing crypto require (caused 'crypto is not defined' error)

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http      = require('http');
 const { Server } = require('socket.io');
 const axios     = require('axios');
 const path      = require('path');
+const crypto    = require('crypto');
 
 // ══════════════════════════════════════════════════
 //  CONFIG
